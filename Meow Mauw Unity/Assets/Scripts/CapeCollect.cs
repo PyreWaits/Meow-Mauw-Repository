41daf30508207526eacaf3486cfd5fa3db531cdf
PyreWaits <78356601+PyreWaits@users.noreply.github.com>
2021-12-14 12:19:41 +0000

    Moving Platform Issues Resolved
    I had to use a pre made script I bought in the past.
    yay.
    Oasis level creation started and basic cape made for pickups.

---
+++ Meow Mauw Unity/Assets/Scripts/CapeCollect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Used for collecting crowns and adding to the crown variable
+public class CapeCollect : MonoBehaviour
+{
+    //On entering area with script add to crowns and destroy script object
+     public void OnTriggerEnter(Collider other) {
+         //Checks for player tag
+        if(other.gameObject.tag=="Player"){
+            Variables.Capes++;
+            Destroy(gameObject);
+        }
+
+    }
+
+}
--- Meow Mauw Unity/Assets/Scripts/Variables.cs
+++ Meow Mauw Unity/Assets/Scripts/Variables.cs
@@ -6,6 +6,9 @@ public class Variables : MonoBehaviour
 {
   //This will hold variables for use within game
       public static int Crowns;
+      public static int Capes;
+      public static int SunGlasses;
+      public static int Scepters;
  
 
 }
